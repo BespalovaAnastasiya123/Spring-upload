7c7df9e36cbea0fe38ba01167915b3d76168e5fd
BespalovaAnastasiya123 <3537184@gmail.com>
2024-08-15 21:25:34 +0300

    15/08/2024

--- src/main/java/org/itstep/email/EmailController.java
+++ src/main/java/org/itstep/email/EmailController.java
@@ -6,16 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class EmailController {
     @Autowired
     EmailService emailService;
+
     @GetMapping("/sendsimple")
     public String sendSimple(Model model){
         Email email = new Email();
         email.setTo("3537184@gmail.com");
         email.setFrom("3537184@gmail.com");
         email.setSubject("Simple text");
+        email.setText("Simple text");
+        Map<String, Object> properties = new HashMap<>();
         emailService.sendSimpleMessage(email);
         return "success";
     }
